f9b4ed81509658aec7a3a76ff8573729851d5da5
Juliya Boiko <boikojuliya2707@gmail.com>
2023-02-23 12:57:08 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const authRouter = require('./routes/auth');
+require('dotenv').config();
+
+const app = express();
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+// ============================= ROUTES =============================
+app.use('/auth', authRouter);
+// app.use('/apartments', apartmentsRouter);
+// app.use('/cities', citiesRouter);
+// app.use('/orders', ordersRouter);
+// ============================= ROUTES =============================
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Server ERROR' } = err;
+  res.status(status).json({ message });
+});
+
+module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const authRouter = express.Router();
+// const { registerSchema, loginSchema } = require('../../shemas/auth');
+// const { validateBody, authenticate } = require('../../middlewares');
+// const { controllerWrapper } = require('../../helpers');
+const { signupUser } = require('../controllers/auth');
+
+authRouter.post('/signup', signupUser);
+// authRouter.post('/register', validateBody(registerSchema), controllerWrapper(registerUser));
+
+// authRouter.post('/login', validateBody(loginSchema), controllerWrapper(loginUser));
+
+// authRouter.post('/logout', authenticate, controllerWrapper(logoutUser));
+
+module.exports = authRouter;
